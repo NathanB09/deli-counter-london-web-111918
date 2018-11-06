3b5b648b05e210ea9307d888e9738389e317c5d0
Nathan Bell <nathanbell09@gmail.com>
2018-11-06 21:22:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(people)
+  if line.size == 0
+    puts "The line is currently empty."
+  else
+    in_line = ["The line is currently:"]
+    people.each do |person|
+      in_line << "#{people.index(person) + 1}. #{person}"
+    end
+    puts in_line.join(" ")
+  end
+end
